a34fb34555bfde6607d4b6acd27303eb1e436456
keithpitt <me@keithpitt.com>
2011-08-21 18:59:57 +0800

    Configured the project to run specs

--- Specs/main.m
+++ Specs/main.m
@@ -9,10 +9,19 @@
 #import <UIKit/UIKit.h>
 #import <Cedar-iPhone/Cedar.h>
 
+#import "DKCoreData.h"
+
 int main(int argc, char *argv[]) {
+    
+    DKCoreData * coreData = [[DKCoreData alloc] initWithDatabase:@"Specs" bundle:nil];
+    
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
     int retVal = UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
     [pool release];
+    
+    [coreData release];
+    
     return retVal;
+    
 }
